c9a807f8ad5a4a2600eda22688135b1b11473c29
Mark McArthey <mcarthey@gmail.com>
2017-04-25 13:59:40 -0500

    Added Resolve and Use methods to map and resolve concrete types within container

--- Container.cs
+++ Container.cs
@@ -6,21 +6,71 @@ using System.Threading.Tasks;
 
 namespace ReflectIt
 {
+    /// <summary>
+    /// Map from one type to another type
+    /// </summary>
+    /// <remarks>Ultimately a container is a map.  </remarks>
     public class Container
     {
-        public Container For<T>()
+        Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <returns></returns>
+        /// <remarks>Good practice to have both a generic and one that takes generic as parameter</remarks>
+        public ContainerBuilder For<TSource>()
         {
-            return this;
+            return For(typeof(TSource));
         }
 
-        public void Use<T>()
+        public ContainerBuilder For(Type sourceType)
         {
-            throw new NotImplementedException();
+            return new ContainerBuilder(this, sourceType);
         }
 
-        public object Resolve<T>()
+        public TSource Resolve<TSource>()
         {
-            throw new NotImplementedException();
+            return (TSource)Resolve(typeof(TSource));
+        }
+        public object Resolve(Type sourceType)
+        {
+            if (_map.ContainsKey(sourceType))
+            {
+                var destinationType = _map[sourceType];
+                return Activator.CreateInstance(destinationType);
+            }
+            throw new InvalidOperationException("Could not resolve " + sourceType.FullName);
+        }
+
+        public class ContainerBuilder
+        {
+            private readonly Container _container;
+            private readonly Type _sourceType;
+
+            public ContainerBuilder(Container container, Type sourceType)
+            {
+                _container = container;
+                _sourceType = sourceType;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <typeparam name="TDestination"></typeparam>
+            /// <returns></returns>
+            /// <remarks>Good practice to have both a generic and one that takes generic as parameter</remarks>
+            public ContainerBuilder Use<TDestination>()
+            {
+                return Use(typeof(TDestination));
+            }
+            public ContainerBuilder Use(Type destinationType)
+            {
+                // write into the map
+                _container._map.Add(_sourceType, destinationType);
+                return this;
+            }
         }
     }
 }
